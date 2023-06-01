db06d8a73d377922df333a14e6b554b453fdb1bd
Vladimir Kirpichyov <vlasoul@mail.ru>
2023-06-01 03:46:06 +0300

    feat: third part of solutions

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -27,8 +27,21 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  let str = '';
+  if (num % 3 === 0 || num % 5 === 0) {
+    for (let i = 1; i >= 0; i -= 1) {
+      for (let j = 1; j >= 0; j -= 1) {
+        const res = (3 ** i) * (5 ** j);
+        if ((i > 0 || j > 0) && num % res === 0) {
+          str = 'Fizz'.repeat(i) + 'Buzz'.repeat(j);
+          break;
+        }
+      }
+      if (str !== '') break;
+    }
+  }
+  return (str === '') ? num : str;
 }
 
 
@@ -43,8 +56,10 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let val = 1;
+  for (let i = 2; i <= n; i += 1) val *= i;
+  return val;
 }
 
 
@@ -60,8 +75,10 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let val = 0;
+  for (let i = n1; i <= n2; i += 1) val += i;
+  return val;
 }
 
 
@@ -80,8 +97,9 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  const arr = [a, b, c].sort((x, y) => x - y);
+  return arr[0] + arr[1] > arr[2];
 }
 
 
@@ -117,8 +135,15 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const inF = (x1, x2, xx1, xx2) => {
+    const negative = (x1 < xx1 && x1 < xx2 && x2 < xx1 && x2 < xx2)
+      || (x1 > xx1 && x1 > xx2 && x2 > xx1 && x2 > xx2);
+    return !negative;
+  };
+
+  return inF(rect1.left, rect1.left + rect1.width, rect2.left, rect2.left + rect2.width)
+    && inF(rect1.top, rect1.top + rect1.height, rect2.top, rect2.top + rect2.height);
 }
 
 
@@ -148,8 +173,10 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const width = point.x - circle.center.x;
+  const height = point.y - circle.center.y;
+  return (width ** 2 + height ** 2) < circle.radius ** 2;
 }
 
 
@@ -164,8 +191,16 @@ function isInsideCircle(/* circle, point */) {
  *   'abracadabra'  => 'c'
  *   'entente' => null
  */
-function findFirstSingleChar(/* str */) {
-  throw new Error('Not implemented');
+function findFirstSingleChar(str) {
+  let index;
+  for (let i = 0; i < str.length; i += 1) {
+    const val = str.substring(0, i) + str.substring(i + 1);
+    if (val.indexOf(str[i]) === -1) {
+      index = i;
+      break;
+    }
+  }
+  return str[index];
 }
 
 
@@ -191,8 +226,13 @@ function findFirstSingleChar(/* str */) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  let x = a;
+  let y = b;
+  if (y < x) [x, y] = [y, x];
+  const start = (isStartIncluded) ? '[' : '(';
+  const finish = (isEndIncluded) ? ']' : ')';
+  return `${start}${x}, ${y}${finish}`;
 }
 
 
@@ -208,8 +248,9 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  const arr = str.split('');
+  return arr.reverse().join('');
 }
 
 
@@ -225,8 +266,9 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  const arr = String(num).split('');
+  return Number(arr.reverse().join(''));
 }
 
 
